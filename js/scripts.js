8493a50f7ccd0a93f79fffd483a3bfba72828762
snowrider-game <thevehicle.in@gmail.com>
2025-02-04 13:08:56 +0530

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+/*!
+* Start Bootstrap - Blog Post v5.0.9 (https://startbootstrap.com/template/blog-post)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-blog-post/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+function goFullscreen(id) {
+    var element = document.getElementById(id);
+    if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      element.webkitRequestFullScreen();
+    }
+  }
